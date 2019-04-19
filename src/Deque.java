9fc8d0a678102063b9176be856db183676e71c57
Shailesh Shenoy <shenoy.shailesh97@gmail.com>
2019-04-19 12:25:51 +0530

    Initail commit

---
+++ src/Deque.java
@@ -0,0 +1,13 @@
+import java.util.Iterator;
+
+public class Deque<Item> implements Iterable<Item>
+{
+	
+	@Override
+	public Iterator<Item> iterator()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
